a9cc6ecfffba7d9fd62f2d356b3fa3d5f1f296e7
Abel B <abel.boilevin@biblibre.com>
2025-10-16 12:08:20 +0200

    Fix potential null return of getDirectorySize.

--- src/Controller/Admin/IndexController.php
+++ src/Controller/Admin/IndexController.php
@@ -28,7 +28,9 @@ class IndexController extends AbstractActionController
         $path = realpath($path);
         if($path!==false && $path!='' && file_exists($path)){
             foreach(new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path, \FilesystemIterator::SKIP_DOTS)) as $object){
-                $bytestotal += $object->getSize();
+                $objectSize = $object->getSize();
+                if (is_numeric($objectSize))
+                    $bytestotal += $objectSize;
             }
         }
         return $bytestotal;
